948cf8bd9434860a31792f1c06ff5f47c9c7b02a
gaoyuan <yuan.gao@ucloud.cn>
2016-10-26 11:54:13 +0800

    add ggtags

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -117,4 +117,10 @@
 
 (provide 'init-local)
 
+;; ggtags
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
+
 ;;; init-local.el ends here
