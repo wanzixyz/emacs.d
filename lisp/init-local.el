a028e0d824f6111588ec05b592a2cee5e6709835
gaoyuan <yuan.gao@ucloud.cn>
2020-04-30 17:47:36 +0800

    add google translate

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -208,6 +208,12 @@
             (when (derived-mode-p 'go-mode)
               (ggtags-mode 1))))
 
+;; google translate
+(require 'google-translate)
+(setq google-translate-default-source-language "en")
+(setq google-translate-default-target-language "zh-CN")
+(global-set-key "\C-ct" 'google-translate-at-point)
+(global-set-key "\C-cT" 'google-translate-query-translate)
 
 ;; zoom
 (custom-set-variables
